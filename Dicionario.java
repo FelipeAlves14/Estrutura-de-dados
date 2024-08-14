e78cfd97882e197c80c2c19ec3d1c427a0210013
Lipeeeeeee <favasconcelos09@gmail.com>
2024-08-14 14:36:55 -0300

    Mudei uns bgl ai, confio no seu potencial :D

--- Dicionario.java
+++ Dicionario.java
@@ -2,20 +2,23 @@ public class Dicionario{
     public class Avaliable{ // Classe avaliable para inserir no lugar de objetos removidos
         public Avaliable(){}
     }
-    public class BaseException extends RuntimeException{ // Exceção pra tudo (preguiça de criar mais de uma)
-        public BaseException(String err){
+    public class NO_SUCH_KEY extends RuntimeException{ // Criei vergonha E uma exceção normal
+        public NO_SUCH_KEY(String err){
             super(err);
         }
     }
     private int n = 0;
     private Object[] data = new Object[10]; // vai começar com 10 elementos pq sim :D
-    private Object binarySearch(int key, int min, int max){
-        int meio = (min + max) / 2;
-        if(data[meio] == data[key]) return data[key];
-        if(min > max) throw new BaseException("Tem esse trem aq não KKKKKKKKKKKKKKKKK");
-        if(data[key] > data[meio]) binarySearch(key, meio, max);
-        else binarySearch(key, min, meio-1);
-    }
+
+    // Notei q não faz sentido ter busca binária se não tiver ordenado, mas deixei ai comentado caso c queira mexer
+
+    // private Object binarySearch(int key, int min, int max){
+    //     int meio = (min + max) / 2;
+    //     if(data[meio] == data[key]) return data[key];
+    //     if(min > max) throw new NO_SUCH_KEY("Tem esse trem aq não KKKKKKKKKKKKKKKKK");
+    //     if(data[key] > data[meio]) binarySearch(key, meio, max);
+    //     else binarySearch(key, min, meio-1);
+    // }
     public int size(){
         return n;
     }
@@ -36,6 +39,22 @@ public class Dicionario{
         return elements;
     }
     public Object findElement(int key){
-        return binarySearch(key, 0, n-1);
+        // tirei a busca binária, confio no seu potencial :D
+    }
+    public void insert(Object obj){
+        int element = (int)obj; // conversão pra calcular na função
+        int index = data.length % element; // função provisória, quiser trocar fique a vontade
+        if(n == datas.length - 1){
+            // rehash (preguiça de fazer)
+        }
+        while(data[index] != null && !data[index].equals(Avaliable)) index = ++index % data.length;
+        /*  
+        se tiver vazio pula o while e é dentro
+        caso contrário vai modificando o index pelo Linear Probing até achar um espaço vazio
+        */
+        data[index] = obj;
+    }
+    public Object removeElement(Object obj){
+        // confio no seu potencial :D
     }
 }
