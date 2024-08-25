de30d75d8bacd779af1f4e3450c8b5b92e30a2da
Lipeeeeeee <favasconcelos09@gmail.com>
2024-08-25 16:38:40 -0300

    Dicionário implementado (em fase de testes)

--- Dicionario.java
+++ Dicionario.java
@@ -39,22 +39,37 @@ public class Dicionario{
         return elements;
     }
     public Object findElement(int key){
-        // tirei a busca binária, confio no seu potencial :D
+        if(key >= n) throw new NO_SUCH_KEY("Tem essa chave aq não po KKKKKKKKKKKKKKKKKKKKKK");
+        return data[key];
     }
     public void insert(Object obj){
+        if(n == data.length - 1){ // rehash
+            Object[] new_data = new Object[data.length * 2]; // novo array pra armazenar elementos
+            Object[] elements = Elements(); // array só com os elementos
+            for(int i = 0; i < n; ++i){ // inserção normal com nova função 
+                int element = (int)elements[i];
+                int index = new_data.length % element;
+                while(new_data[index] != null) index = ++index % new_data.length;
+                new_data[index] = elements[i];       
+            }
+            data = new_data;
+        }
         int element = (int)obj; // conversão pra calcular na função
         int index = data.length % element; // função provisória, quiser trocar fique a vontade
-        if(n == data.length - 1){
-            // rehash (preguiça de fazer)
-        }
         while(data[index] != null && !(data[index] instanceof Avaliable)) index = ++index % data.length;
         /*  
         se tiver vazio pula o while e é dentro
         caso contrário vai modificando o index pelo Linear Probing até achar um espaço vazio
         */
         data[index] = obj;
+        ++n;
     }
-    public Object removeElement(Object obj){
-        // confio no seu potencial :D
+    public Object removeElement(int key){
+        if(key >= n) throw new NO_SUCH_KEY("Tem essa chave aq não po KKKKKKKKKKKKKKKKKKKKKK");
+        Object retorno = data[key];
+        if(retorno instanceof Avaliable || retorno == null) throw new NO_SUCH_KEY("Tem nada aq não po vai remover oq KKKKKKKKKKKKKKKKKKKKKK");
+        data[key] = new Avaliable();
+        --n;
+        return retorno;
     }
 }
