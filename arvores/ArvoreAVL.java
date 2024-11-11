2f00005b5ca487b73074dd8ff3f702fcf3c07aa2
Felipe Alves de Vasconcelos <20231014040027@ifrn.local>
2024-11-11 10:17:01 -0300

    feat: adicionando esboço do método remove

--- arvores/ArvoreAVL.java
+++ arvores/ArvoreAVL.java
@@ -3,7 +3,7 @@ public class ArvoreAVL extends ArvoreDePesquisa {
     @Override
     protected void inOrder(No atual){
         if(hasLeft(atual)) inOrder(leftChild(atual));
-        System.out.print(atual.element + "[" + atual.fb + "]");
+        System.out.print(atual.element + "[" + atual.fb + "]    ");
         if(hasRight(atual)) inOrder(rightChild(atual));
     }
 
@@ -60,21 +60,12 @@ public class ArvoreAVL extends ArvoreDePesquisa {
     }
 
     @Override
-    public int height(No atual){
-        if(isExternal(atual)) return depth(atual);
-        int leftHeight = 0, rightHeight = 0;
-        if(hasLeft(atual)) leftHeight = height(leftChild(atual));
-        if(hasRight(atual)) rightHeight = height(rightChild(atual));
-        return (leftHeight > rightHeight) ? leftHeight : rightHeight;
-    }
-
-    @Override
     public void insert(Object elem){
         No novo = new No(elem);
         if(isEmpty()) raiz = novo;
         else{
             No pai = find(elem, raiz);
-            if(pai.element == elem) throw new BaseException("Elemento ja existe na Árvore");
+            if(pai.element == elem) throw new BaseException("Elemento já existe na Árvore");
             else{
                 if((int)elem < (int)pai.element) pai.lChild = novo;
                 else pai.rChild = novo;
@@ -99,4 +90,82 @@ public class ArvoreAVL extends ArvoreDePesquisa {
         }
         ++n;
     }
+
+    // @Override
+    // public No remove(Object elem){
+    //     if(isEmpty()) throw new BaseException("Árvore vazia, não há elementos");
+    //     No no = find(elem, raiz);
+    //     No retorno = no;
+    //     No paiRemovido = null;
+    //     if(no.element != elem) throw new BaseException("Elemento não existe na Árvore");
+    //     boolean ehRaiz = isRoot(no);
+    //     No pai = null;
+    //     if(!ehRaiz){
+    //         pai = parent(no);
+    //         paiRemovido = pai;
+    //     }
+    //     if(isExternal(no)){
+    //         if(ehRaiz) raiz = null;
+    //         else{
+    //             No esquerdo = leftChild(pai);
+    //             if(esquerdo == no) pai.lChild = null;
+    //             else pai.rChild = null;
+    //         }
+    //     }
+    //     else if(hasLeft(no) && !hasRight(no) || !hasLeft(no) && hasRight(no)){
+    //         if(hasLeft(no)){
+    //             if(ehRaiz){
+    //                 raiz = leftChild(no);
+    //                 raiz.parent = null;
+    //             }
+    //             else{
+    //                 No filhoEsquerdo = leftChild(no);
+    //                 filhoEsquerdo.parent = pai;
+    //                 pai.lChild = filhoEsquerdo;
+    //             }
+    //         }
+    //         else{
+    //             if(ehRaiz){
+    //                 raiz = rightChild(no);
+    //                 raiz.parent = null;
+    //             }
+    //             else{
+    //                 No filhoDireito = rightChild(no);
+    //                 filhoDireito.parent = pai;
+    //                 pai.rChild = filhoDireito;
+    //             }
+    //         }
+    //     }
+    //     else{
+    //         No substituto = rightChild(no);
+    //         while(hasLeft(substituto)) substituto = leftChild(substituto);
+    //         no.element = substituto.element;
+    //         pai = parent(substituto);
+    //         if(hasRight(substituto)){
+    //             substituto = rightChild(substituto);
+    //             substituto.parent = pai;
+    //             pai.rChild = substituto;
+    //         }
+    //         else{
+    //             if(substituto == leftChild(pai)) pai.lChild = null;
+    //             else pai.rChild = null;
+    //         }
+    //     }
+    //     --n;
+    //     No atual = paiRemovido;
+    //     while(!isRoot(atual)){
+    //         if(leftChild(pai) == atual) pai.fb--;
+    //         else if(rightChild(pai) == atual) pai.fb++;
+    //         if(pai.fb == 2){
+    //             if(atual.fb < 0) atual = rotacaoDuplaDireita(pai);
+    //             else atual = rotacaoSimplesDireita(pai);
+    //         }
+    //         else if(pai.fb == -2){
+    //             if(atual.fb > 0) atual = rotacaoDuplaEsquerda(pai);
+    //             else atual = rotacaoSimplesEsquerda(pai);
+    //         }
+    //         if(pai.fb != 0) break;
+    //     }
+    //     return retorno;
+    // }
 }
