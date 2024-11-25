f6bf0d2dbe18dfe0a4b37e51ef03baae7a4d6a8b
Felipe Alves de Vasconcelos <20231014040027@ifrn.local>
2024-11-25 14:08:33 -0300

    feat: Situação 1 e situação 2 da remoção adicionada, começo da situação 3

--- arvores/ArvoreRubroNegra.java
+++ arvores/ArvoreRubroNegra.java
@@ -1,11 +1,34 @@
 package arvores;
 public class ArvoreRubroNegra extends ArvoreDePesquisa{
     
+    private void caso1(No no){
+
+    }
+
+    private 
+
+    private void situacao3(No no){
+        No pai = parent(no);
+        No irmao = no == leftChild(pai) ? rightChild(pai) : leftChild(pai);
+        if(irmao.cor == "Rubro" && pai.cor == "Negro") caso1(pai);
+        else{
+            No sobrinhoPerto = no == leftChild(pai) ? leftChild(irmao) : rightChild(irmao);
+            No sobrinhoLonge = no == leftChild(pai) ? rightChild(irmao) : leftChild(irmao);
+            if(irmao.cor == "Negro" && sobrinhoLonge.cor == "Negro" && sobrinhoPerto.cor == "Negro"){ // casos 2
+                if(pai.cor == "Rubro") pai.cor = "Negro"; // caso 2B
+                irmao.cor = "Rubro"; // ambos casos 2 o irmão vira rubro
+            }
+        }
+    }
+
+    private void situacao4(No no){
+        
+    }
+
     @Override
     protected void inOrder(No atual){
         if(hasLeft(atual)) inOrder(leftChild(atual));
-        System.out.print(atual.element + "[" + atual.cor + "]    ");
-        if(atual.duploNegro != 0) System.out.print("-> " + atual.duploNegro);
+        System.out.print(atual.element + "[" + atual.cor + (atual.duploNegro != 0 ? " -> Duplo negro: " + atual.duploNegro : "") + "]   ");
         if(hasRight(atual)) inOrder(rightChild(atual));
     }
 
@@ -158,22 +181,28 @@ public class ArvoreRubroNegra extends ArvoreDePesquisa{
                 if(ehRaiz){
                     raiz = leftChild(no);
                     raiz.parent = null;
+                    raiz.cor = "Negro";
                 }
                 else{
                     No filhoEsquerdo = leftChild(no);
                     filhoEsquerdo.parent = pai;
-                    pai.lChild = filhoEsquerdo;
+                    if(leftChild(pai) == no) pai.lChild = filhoEsquerdo;
+                    else pai.rChild = filhoEsquerdo;
+                    if(retorno.cor == "Negro" && filhoEsquerdo.cor == "Rubro") filhoEsquerdo.cor = "Negro";
                 }
             }
             else{
                 if(ehRaiz){
                     raiz = rightChild(no);
                     raiz.parent = null;
+                    raiz.cor = "Negro";
                 }
                 else{
                     No filhoDireito = rightChild(no);
                     filhoDireito.parent = pai;
-                    pai.rChild = filhoDireito;
+                    if(leftChild(pai) == no) pai.lChild = filhoDireito;
+                    else pai.rChild = filhoDireito;
+                    if(retorno.cor == "Negro" && filhoDireito.cor == "Rubro") filhoDireito.cor = "Negro";
                 }
             }
         }
@@ -183,15 +212,23 @@ public class ArvoreRubroNegra extends ArvoreDePesquisa{
             no.element = substituto.element;
             pai = parent(substituto);
             if(hasRight(substituto)){
-                substituto = rightChild(substituto);
-                substituto.parent = pai;
-                if(leftChild(pai).element == retorno.element) pai.lChild = substituto;
-                else pai.rChild = substituto;
-
+                No filhoDireito = rightChild(substituto);
+                if(substituto.cor == "Negro" && filhoDireito.cor == "Rubro") filhoDireito.cor = "Negro";
+                else if(substituto.cor == "Negro" && filhoDireito.cor == "Negro") situacao3(substituto);
+                filhoDireito.parent = pai;
+                if(leftChild(pai).element == retorno.element) pai.lChild = filhoDireito;
+                else pai.rChild = filhoDireito;
             }
             else{
-                if(substituto == leftChild(pai)) pai.lChild = nulo;
-                else pai.rChild = nulo;
+                if(substituto == leftChild(pai)){
+                    pai.lChild = nulo;
+                    if(substituto.cor == "Negro") pai.duploNegro = 1;
+                }
+                else{
+                    pai.rChild = nulo;
+                    if(substituto.cor == "Negro") pai.duploNegro = -1;
+                }
+
             }
         }
         --n;
